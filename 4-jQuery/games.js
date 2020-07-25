3124a1a6d0e08164ab02cc185ccd2f960e203e34
CutieMath <cutie.math@protonmail.com>
2020-07-25 21:40:19 +1000

    finished jQuery & added to my main site

--- 4-jQuery/games.js
+++ 4-jQuery/games.js
@@ -1,6 +1,18 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var start = false;
+var level = 0;
+
+// When a keyboard key has been pressed, invoke nextSequence
+$(document).keypress(function(){
+  if(!start) {
+    $("#level-title").text("Level  " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 
 // Check which button is pressed
 $(".btn").click(function() {
@@ -10,16 +22,45 @@ $(".btn").click(function() {
   playSound(userChosenColour);
   // add color to pressed button
   animatePress(userChosenColour);
-
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    if(userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    // change background
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    // change title
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+    startOver();
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+  level ++;
+  $("#level-title").text("Level  " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   // animate flash
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   // play sound
   playSound(randomChosenColour);
@@ -37,5 +78,10 @@ function animatePress(currentColour) {
       $("#" + currentColour).removeClass("pressed");
     }, 150);
     console.log(currentColour);
+}
 
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
