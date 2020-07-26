4a5c5ba96c76ba9479f695e6b28a25fa7c60e481
CutieMath <cutie.math@protonmail.com>
2020-07-26 16:43:41 +1000

    add npm packages

---
+++ 5-nodeJS/index.js
@@ -0,0 +1,6 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+var myName = superheroes.random();
+var myNickName = supervillains.random();
+
+console.log(myName + " NickName: " + myNickName);
