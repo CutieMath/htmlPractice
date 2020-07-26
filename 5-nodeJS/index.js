f176f788d636a344ce9ed1c22109ec42635048da
CutieMath <cutie.math@protonmail.com>
2020-07-26 21:00:40 +1000

    making a calculator using express

--- 5-nodeJS/index.js
+++
@@ -1,6 +0,0 @@
-var superheroes = require("superheroes");
-var supervillains = require("supervillains");
-var myName = superheroes.random();
-var myNickName = supervillains.random();
-
-console.log(myName + " NickName: " + myNickName);
