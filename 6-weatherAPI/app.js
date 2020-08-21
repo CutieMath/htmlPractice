4243988b30e999cd6a027ac4d5f83046bc588699
CutieMath <cutie.math@protonmail.com>
2020-08-21 22:40:30 +1000

    finished weather API

--- 6-weatherAPI/app.js
+++ 6-weatherAPI/app.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?appid=8a8957157216d804cdf7214e4674fd2a&q=London,uk&units=metric"
+  res.sendFile(__dirname + "/index.html");
+
+})
+
+app.post("/", function(req, res) {
+  const query = req.body.cityInputName
+  const url = "https://api.openweathermap.org/data/2.5/weather?appid=8a8957157216d804cdf7214e4674fd2a&q=" + query + ",uk&units=metric"
   https.get(url, function(response){
     console.log(response.statusCode);
     response.on("data", function(data){
       const weatherData = JSON.parse(data)
-      const temp = weatherData.weather[0].description
-      console.log(temp);
+      const temp = weatherData.main.temp
+      const weather = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<h1>The weather in " + query + " is " + weather + "!<br>");
+      res.write("The temperature is fucking " + temp + " degrees Celcius.</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send()
     })
   })
-  res.send("Server is up and running")
 })
+
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.")
