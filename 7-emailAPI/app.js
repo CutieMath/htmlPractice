19ed5ae25fce58156fd62b60ffc5500aa99a89eb
CutieMath <cutie.math@protonmail.com>
2020-08-23 14:35:11 +1000

    finished sign up forms

--- 7-emailAPI/app.js
+++ 7-emailAPI/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 const app = express();
 // to get local css to work
 app.use(express.static("public"));
@@ -11,13 +12,44 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-
-  console.log(firstName, lastName, email);
-
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us17.api.mailchimp.com/3.0/lists/5d6a5941f1";
+  const options = {
+    method: "POST",
+    auth: "cutie:2603fbd6f8b52b3bb6fd005b7c3076d9-us1",
+  }
+  const request = https.request(url, options, function(response) {
+    if(response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    }else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
 });
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
 
 
 app.listen(3000, function() {
