9d67069f263aa922b5b40e148d146310eb927a4f
CutieMath <cutie.math@protonmail.com>
2020-08-31 12:18:14 +1000

    finish

--- 9-gratitudeJournal/app.js
+++ 9-gratitudeJournal/app.js
@@ -64,6 +64,6 @@ app.get("/posts/:name", function(req, res) {
   res.end();
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
