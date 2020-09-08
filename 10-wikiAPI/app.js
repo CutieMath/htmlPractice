9c7b3871bece761a0f87edfbbfeea68e18387cb6
CutieMath <cutie.math@protonmail.com>
2020-09-08 16:04:44 +1000

    create API using Node.js

---
+++ 10-wikiAPI/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+// setup ejs
+app.set('view engine', 'ejs');
+// setup body parser
+app.use(bodyParser.urlencoded({extended: true}));
+// allow local css
+app.use(express.static("public"));
+
+// connect mongodb
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost:27017/wikiDB', { useUnifiedTopology: true });
+
+// setup db collections
+const articleSchema = {
+  title: String,
+  content: String
+};
+const Article = mongoose.model("Article", articleSchema);
+
+
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticles){
+    if(!err){
+        res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+app.post("/articles", function(req, res) {
+  const newArticle = new Article ({
+      title: req.body.title,
+      content: req.body.content
+  });
+  newArticle.save(function(err) {
+    if(!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+app.delete("/articles", function(req, res) {
+  Article.deleteMany(function(err){
+      if(!err){
+        res.send("Successfully deleted everything.");
+      } else {
+        res.send(err);
+      }
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000 x");
+});
