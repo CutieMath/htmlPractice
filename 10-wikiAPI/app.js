062ad84bc38608ad6ca69f27ba3ff0e37b1eb13a
CutieMath <cutie.math@protonmail.com>
2020-09-08 16:14:47 +1000

    add route chaining

--- 10-wikiAPI/app.js
+++ 10-wikiAPI/app.js
@@ -24,41 +24,40 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
-app.get("/articles", function(req, res) {
-  Article.find(function(err, foundArticles){
-    if(!err){
-        res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-
-app.post("/articles", function(req, res) {
-  const newArticle = new Article ({
-      title: req.body.title,
-      content: req.body.content
-  });
-  newArticle.save(function(err) {
-    if(!err){
-      res.send("Successfully added a new article.");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-
-app.delete("/articles", function(req, res) {
-  Article.deleteMany(function(err){
+app.route("/articles")
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles){
       if(!err){
-        res.send("Successfully deleted everything.");
+          res.send(foundArticles);
+      } else {
+          res.send(err);
+      }
+    });
+  })
+
+  .post(function(req, res) {
+    const newArticle = new Article ({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if(!err){
+        res.send("Successfully added a new article.");
       } else {
         res.send(err);
       }
-  });
-});
+    });
+  })
+  
+  .delete(function(req, res) {
+    Article.deleteMany(function(err){
+        if(!err){
+          res.send("Successfully deleted everything.");
+        } else {
+          res.send(err);
+        }
+     });
+   });
 
 
 app.listen(3000, function() {
