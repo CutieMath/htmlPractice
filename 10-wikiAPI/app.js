70c6ca64a3b4fadce6c24ec14eeca49c3f023c77
CutieMath <cutie.math@protonmail.com>
2020-09-10 20:30:36 +1000

    add CRUD functions target specific articles

--- 10-wikiAPI/app.js
+++ 10-wikiAPI/app.js
@@ -24,6 +24,7 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
+// Requests targeting all articles
 app.route("/articles")
   .get(function(req, res) {
     Article.find(function(err, foundArticles){
@@ -48,7 +49,7 @@ app.route("/articles")
       }
     });
   })
-  
+
   .delete(function(req, res) {
     Article.deleteMany(function(err){
         if(!err){
@@ -59,6 +60,49 @@ app.route("/articles")
      });
    });
 
+
+// Requests targeting a specific article
+app.route("/articles/:articleTitle")
+  // receive an article
+  .get(function(req, res) {
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+      if(foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching the title was found.");
+      }
+    });
+  })
+  // change an entry
+  .put(function(req, res) {
+    Article.update({title: req.params.articleTitle}, {title: req.body.title, content: req.body.content}, {overwrite: true},
+                   function(err, results) {
+                      if(!err) {
+                        res.send("Successfully updated article.");
+                      }
+                   });
+  })
+  // change a specific value within an entry
+  // this step can also be dynamic (use req.body)
+  .patch(function(req, res) {
+    Article.update({title: req.params.articleTitle}, {$set: req.body}, function(err){
+      if(!err) {
+        res.send("Successfully updated record.")
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  // delete a specific article
+  .delete(function(req, res) {
+    Article.deleteOne({title: req.params.articleTitle}, function(err) {
+      if(!err) {
+        res.send("Successfully deleted the article.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000 x");
