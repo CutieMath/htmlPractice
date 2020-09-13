6ccbce045fb3ac8ca73ce4a74a50a6d93fa1da79
CutieMath <cutie.math@protonmail.com>
2020-09-13 21:58:23 +1000

    fnish level one authentication: read user email and password

--- b-wisperAuth/app.js
+++ b-wisperAuth/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const app = express();
 
 // setup ejs
@@ -9,6 +10,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 // allow local css
 app.use(express.static("public"));
+
+// connect to mongodb
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.connect("mongodb://localhost:27017/userDB", { useUnifiedTopology: true });
+
+// set up userDB
+const userSchema = {
+  email: String,
+  password: String
+};
+const User = new mongoose.model("User", userSchema);
 
 
 app.get("/", function(req, res) {
@@ -23,6 +36,36 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.post("/register", function(req, res) {
+  // create user and store in mongodb
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if(!err){
+      res.render("secrets");
+    }else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else {
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000 x")
