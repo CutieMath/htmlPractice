4c97620586724247868451d1bee57944eb1d34e1
CutieMath <cutie.math@protonmail.com>
2020-10-19 20:27:36 +1100

    add mapping components in React

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Header from './Header';
-import Note from './Note';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import notes from "../notes";
 
 function App(){
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((input) => (
+                <Note 
+                    key={input.id}
+                    title={input.title}
+                    content={input.content}
+                />
+            ))}
             <Footer />
         </div>
     );
---
+++ my-app/src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+    {
+      key: 1,
+      title: "٩(◕‿◕｡)۶",
+      content:
+        "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+    },
+    {
+      key: 2,
+      title: "o(>ω<)o",
+      content:
+        "Q. How to keep a programmer in the shower forever? A. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+    },
+    {
+      key: 3,
+      title: "(✧ω✧)",
+      content:
+        "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+    },
+    {
+      key: 4,
+      title: "ヽ(o^ ^o)ﾉ",
+      content:
+        "Babe x"
+    }
+  ];
+
+  export default notes;
